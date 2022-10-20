fc171e49ae9d421f3e01899063d9781e7c10e4ec
Tyler Bedard <tylerbedard126@gmail.com>
2022-10-19 22:17:07 -0400

    beginning HTML,JS and CSS

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+const startBtn = document.getElementById('start-btn');
+
+startBtn.addEventListener('click', startGame);
+
+// let div = document.getElementById("div")
+
+function startGame() {
+    console.log("farted")
+    // document.createElement("div")
+    // div.textContent = "this is  the first question"
+    // document.body.appendChild(div)
+
+}
+
+const question = {
+
+}
+
+// function redBox() {
+//     var div = document.createElement("div");
+//     div.style.width = "100px";
+//     div.style.height = "100px";
+//     div.style.background = "red";
+//     div.style.color = "white";
+//     div.innerHTML = "Hello";
+// }
+
+
+// redBox()
+// document.getElementById("main").appendChild(div);
